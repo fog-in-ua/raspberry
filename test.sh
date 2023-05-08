d4a9781a59aa6f2275d438087673c21727fb24e1
fog.in.ua <120197804+fog-in-ua@users.noreply.github.com>
2023-05-08 15:22:37 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,12 @@
 sudo docker run \
    --name zigbee2mqtt \
    --restart=unless-stopped \
-   --device=/dev/ttyUSB0 \
+#   --device=/dev/ttyUSB0 \
+   --device=/dev/serial/by-id/usb-ITEAD_SONOFF_Zigbee_3.0_USB_Dongle_Plus_V2_20221201165558-if00:/dev/ttyACM0 \
    -p 8082:8082 \
    -v $(pwd)/data:/app/data \
    -v /run/udev:/run/udev:ro \
    -e TZ=Europe/Kyiv \
    koenkk/zigbee2mqtt
 # curl -sk https://raw.githubusercontent.com/fog-in-ua/raspberry/main/test.sh | sudo bash -
+# usb-ITEAD_SONOFF_Zigbee_3.0_USB_Dongle_Plus_V2_20221201165558-if00 -> ../../ttyACM0
