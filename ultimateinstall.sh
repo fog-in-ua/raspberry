281fc96efb4776b5b3f85dc2c22e71bf0e0e4540
fog.in.ua <120197804+fog-in-ua@users.noreply.github.com>
2023-05-08 09:14:20 +0300

    Update ultimateinstall.sh

--- ultimateinstall.sh
+++ ultimateinstall.sh
@@ -29,6 +29,20 @@ echo "----------------------------------------------------------------"
 echo " "
 echo " "
 echo " "
+# Homebridge setup
+echo "----------------------------------------------------------------"
+echo "Commence Homebridge Setup"
+echo "----------------------------------------------------------------"
+curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
+sudo apt-get update
+sudo apt-get install homebridge
+echo "----------------------------------------------------------------"
+echo "Homebridge Interface is reachable at homebridge.local:8581""
+echo "----------------------------------------------------------------"
+echo " "
+echo " "
+echo " "
 # Docker setup
 echo "----------------------------------------------------------------"
 echo "Commence Docker Setup"
