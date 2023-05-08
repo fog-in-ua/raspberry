1ef36fe5ee14d0d8552742d234e25c24135919eb
fog.in.ua <120197804+fog-in-ua@users.noreply.github.com>
2023-05-08 14:27:02 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@
 sudo docker run \
    --name zigbee2mqtt \
    --restart=unless-stopped \
-   --device=/dev/ttyACM0 \
-   -p 8080:8080 \
+   --device=/dev/ttyUSB0 \
+   -p 8082:8082 \
    -v $(pwd)/data:/app/data \
    -v /run/udev:/run/udev:ro \
-   -e TZ=Europe/Amsterdam \
+   -e TZ=Europe/Kyiv \
    koenkk/zigbee2mqtt
 # curl -sk https://raw.githubusercontent.com/fog-in-ua/raspberry/main/test.sh | sudo bash -
