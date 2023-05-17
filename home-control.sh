439ca43a7aba3884c827c01d70dda446958aaef6
fog.in.ua <120197804+fog-in-ua@users.noreply.github.com>
2023-05-17 16:25:35 +0300

    Update home-control.sh

--- home-control.sh
+++ home-control.sh
@@ -1,1 +1,53 @@
+#!/bin/bash
 
+##################################################################################################
+# Редакція для Rasberry Pi                                       Copyright (C) 2023 Ihor Koliukh #
+#                                                                                                #
+# Ця програма є безкоштовним програмним забезпеченням: ви можете поширювати її та/або            #
+# змінювати згідно з умовами GNU General Public License, опублікованої Free Software             #
+# Foundation, версії 3 Ліцензії або (на ваш вибір) будь-якої пізнішої версії.                    #                                                           #
+#                                                                                                #
+# Ця програма розповсюджується в надії, що вона буде корисною, але БЕЗ ЖОДНИХ ГАРАНТІЙ; навіть   #
+# без неявної гарантії ПРИДАТНОСТІ ДЛЯ ПРОДАЖУ чи ВІДПОВІДНОСТІ ДЛЯ КОНКРЕТНОЇ МЕТИ. Додаткову   #
+# інформацію дивись у загальній публічній ліцензії GNU.                                          #
+#                                                                                                #
+# Ви мали отримати копію GNU General Public License разом із цією програмою.                     #
+# Якщо ні, див <http://www.gnu.org/licenses/>.                                                   #
+##################################################################################################
+echo " "
+echo " "
+# System Upgrade
+echo "----------------------------------------------------------------"
+echo "Commence System Upgrade"
+echo "----------------------------------------------------------------"
+#sudo apt-get update && sudo apt-get upgrade -y
+#sudo rpi-eeprom-update -d -a
+echo "----------------------------------------------------------------"
+echo "System Upgrade Completed"
+echo "----------------------------------------------------------------"
+echo " "
+echo " "
+# MC setup
+echo "----------------------------------------------------------------"
+echo "Commence MC Setup"
+echo "----------------------------------------------------------------"
+#sudo apt-get update
+#sudo apt-get install -y mc
+echo "----------------------------------------------------------------"
+echo "MC Setup Completed"
+echo "----------------------------------------------------------------"
+echo " "
+echo " "
+# Homebridge setup
+echo "----------------------------------------------------------------"
+echo "Commence Homebridge Setup"
+echo "----------------------------------------------------------------"
+curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y homebridge
+echo "----------------------------------------------------------------"
+echo "Homebridge Interface is reachable at homebridge.local:8581"
+echo "----------------------------------------------------------------"
+echo " "
+echo " "
