2650d5a49f05480da62e8672c722688215ba692c
fog.in.ua <120197804+fog-in-ua@users.noreply.github.com>
2023-05-17 21:52:12 +0300

    Update home-control.sh

--- home-control.sh
+++ home-control.sh
@@ -49,25 +49,25 @@ echo "   MC Setup Completed"
 echo "----------------------------------------------------------------"
 echo " "
 echo " "
-#echo "----------------------------------------------------------------"
-#echo "  Homebridge Setup"
-#echo "----------------------------------------------------------------"
-#curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
-#echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
-#sudo apt-get update
-#sudo apt-get install -y homebridge
-#echo "----------------------------------------------------------------"
-#echo "  Homebridge Interface is reachable at homebridge.local:8581"
-#echo "----------------------------------------------------------------"
+echo "----------------------------------------------------------------"
+echo "  Homebridge Setup"
+echo "----------------------------------------------------------------"
+curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y homebridge
+echo "----------------------------------------------------------------"
+echo "  Homebridge Interface is reachable at homebridge.local:8581"
+echo "----------------------------------------------------------------"
 echo " "
 echo " "
-#echo "----------------------------------------------------------------"
-#echo "  Scypted Setup"
-#echo "----------------------------------------------------------------"
-
-#echo "----------------------------------------------------------------"
-#echo "  Scrypted Interface is reachable at https://localhost:10443"
-#echo "----------------------------------------------------------------"
+echo "----------------------------------------------------------------"
+echo "  Scypted Setup"
+echo "----------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/koush/scrypted/main/install/local/install-scrypted-dependencies-linux.sh | sudo SERVICE_USER=$USER bash
+echo "----------------------------------------------------------------"
+echo "  Scrypted Interface is reachable at https://localhost:10443"
+echo "----------------------------------------------------------------"
 echo " "
 echo " "
 echo "----------------------------------------------------------------"
@@ -94,8 +94,8 @@ echo " "
 ##echo "----------------------------------------------------------------"
 #echo "  Portainer Interface is reachable at homebridge.local:9000"
 #echo "----------------------------------------------------------------"
-echo " "
-echo " "
+#echo " "
+#echo " "
 echo "----------------------------------------------------------------"
 echo "   Portainer Setup in Docker"
 echo "----------------------------------------------------------------"
@@ -114,28 +114,15 @@ echo "   Watch Tower Setup Completed"
 echo "----------------------------------------------------------------"
 echo " "
 echo " "
-echo "----------------------------------------------------------------"
-echo "  Homebridge Setup"
-echo "----------------------------------------------------------------"
-sudo docker run --net=host --name=homebridge -v $(pwd)/homebridge:/homebridge oznu/homebridge:latest
-version: '2'
-services:
-  homebridge:
-    image: oznu/homebridge:latest
-    restart: always
-    network_mode: host
-    volumes:
-      - ./volumes/homebridge:/homebridge
-    logging:
-      driver: json-file
-      options:
-        max-size: "10mb"
-        max-file: "1"
-echo "----------------------------------------------------------------"
-echo "  Homebridge Interface is reachable at homebridge.local:8581"
-echo "----------------------------------------------------------------"
-echo " "
-echo " "
+#echo "----------------------------------------------------------------"
+#echo "  Homebridge Setup"
+#echo "----------------------------------------------------------------"
+#sudo docker run --net=host --name=homebridge -v $(pwd)/homebridge:/homebridge oznu/homebridge:latest
+#echo "----------------------------------------------------------------"
+#echo "  Homebridge Interface is reachable at homebridge.local:8581"
+#echo "----------------------------------------------------------------"
+#echo " "
+#echo " "
 echo "----------------------------------------------------------------"
 echo "   HEIMDALL Setup in Docker"
 echo "----------------------------------------------------------------"
