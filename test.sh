d6fa26ca9c67325ad63b3076d63543886f35b5ba
fog.in.ua <120197804+fog-in-ua@users.noreply.github.com>
2023-05-17 17:32:25 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,3 +12,17 @@ sudo docker run \
    koenkk/zigbee2mqtt
 # curl -sk https://raw.githubusercontent.com/fog-in-ua/raspberry/main/test.sh | sudo bash -
 # usb-ITEAD_SONOFF_Zigbee_3.0_USB_Dongle_Plus_V2_20221201165558-if00 -> ../../ttyACM0
+
+
+
+
+sudo apt update
+sudo apt upgrade
+sudo apt install mosquitto mosquitto-clients
+
+
+
+sudo systemctl status mosquitto
+# Вы должны увидеть текст "active (running)", если служба запущена правильно.
+# https://pimylifeup.com/raspberry-pi-mosquitto-mqtt-server/
+
