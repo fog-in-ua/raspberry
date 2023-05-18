83f0df55dabb6202f609ac99abffa6ca7f1356a5
fog.in.ua <120197804+fog-in-ua@users.noreply.github.com>
2023-05-18 11:42:43 +0300

    Update node_update.sh

--- node_update.sh
+++ node_update.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# 
-#
-#
+#############################################################################
+# https://raw.githubusercontent.com/fog-in-ua/raspberry/main/node_update.sh #
+#############################################################################
+
 sudo apt-get update && sudo apt-get upgrade -y 
-
 sudo hb-service update-node
-
 sudo npm install -g npm
-
 sudo reboot
